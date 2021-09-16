45f4dd432312f2126f1d2e56bd93c458425bc8fd
Maithreyi Gopal <maithreyi.gopal@gmail.com>
2021-09-16 22:27:29 +0200

    added file handling and file permission commands

--- masterVarAndFile.sh
+++ masterVarAndFile.sh
@@ -26,7 +26,7 @@ echo $c
 #HOME Variable - stores the credential of the system
 
 VAR_PATH=$HOME
-VAR_CURPATH=$CWD
+VAR_CURPATH=$PWD
 
 echo "Home directory is $VAR_PATH"
 
@@ -36,7 +36,7 @@ cd $VAR_PATH
 echo "check contents of the home directory"
 ls
 
-echo "back to where we started"
+echo -e"\nback to where we started"
 cd $VAR_CURPATH
 
 #USER Variable
@@ -51,7 +51,7 @@ echo "\$HOSTNAME"
 
 #writing a file in the home directory and create a file there
 cd $HOME
-echo "I am creating this file from far away" > test.txt
+echo -e"\nI am creating this file from far away" > test.txt
 
 cd $VAR_CURPATH
 cat $HOME/test.txt
@@ -60,6 +60,40 @@ cat $HOME/test.txt
 touch filetouch.txt
 ls
 
+#creating symbolic link, write to a Linked file and Cat a Linked file
+cd $VAR_CURPATH
+
+rm linkFile
+#create a link to file.txt
+ln -s file.txt linkFile
+ls #only to verify
+
+echo -e "\nNow I am writing inside the link" >>linkFile
+cat linkFile
+
+echo -e "\nLets also check the original file"
+cat file.txt
+
+echo -e "\nmodifying the original file and will verify the link and the file"
+echo "this is my new modification in the original file." >>file.txt
+
+echo -e "\ncat link"
+cat linkFile
+
+echo -e "\nLets also check the original file"
+cat file.txt
+
+
+echo -e "\nNext we make the file executable using chmod! create script.sh and change mod"
+rm script.sh
+touch script.sh
+chmod +x script.sh
+
+ls -la script.sh
+
+echo -e "\nremove the non writable from above"
+chmod -w script.sh
+ls -la script.sh
 
 
 
